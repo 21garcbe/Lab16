f4bfc393233ea6934f3367c7ad9e32dbd29af1dd
21garcbe <21garcbe@gmail.com>
2026-05-04 15:29:18 -0400

    created reader and tested plotting (wip)

--- Lab16-bgarcia15-2.py
+++ Lab16-bgarcia15-2.py
@@ -8,13 +8,34 @@ Author: Ben Garcia
 """
 
 import plotly.express as ex
+import csv
 from pathlib import Path 
 
-class Main():
-    """Plots gobal fire data from csv format using plotly"""
-    #set path to csv of fire data
-    path = Path('world_fires_1_day.csv')
-    #set title and figure to a basic worldmap
-    title = "Global Fires"
-    fig = ex.scatter_geo(lat = lats, lon= lons, title = title)
+
+"""Plots gobal fire data from csv format using plotly"""
+#set path to csv of fire data
+path = Path('world_fires_1_day.csv')
+
+
+lats = []
+lons = []
+
+#read csv
+with path.open() as file:
+    reader = csv.reader(file)
+    row = next(reader)
+    
+    for row in reader:
+        lat = float(row[0])
+        lon = float(row[1])
+
+        lats.append(lat)
+        lons.append(lon)
+
+        
+#set title and figure to a basic worldmap
+title = "Global Fires"
+fig = ex.scatter_geo(lat = lats, lon= lons, title = title)
+fig.show()
+
 
